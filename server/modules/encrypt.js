4e89028341cb2d98d2a077ed6bc8154e135f4c1d
JeaV2 <102710@glr.nl>
2026-06-05 15:59:24 +0200

    feat(auth): add registration functionality

---
+++ server/modules/encrypt.js
@@ -0,0 +1,13 @@
+import bcrypt from 'bcrypt';
+import 'dotenv/config';
+
+const SALT_ROUNDS = parseInt(process.env.PSWD_SALTROUNDS);
+if (isNaN(SALT_ROUNDS)) {
+    throw new Error('SALT_ROUNDS environment variable must be a valid number');
+}
+
+async function hashPassword(password) {
+    return await bcrypt.hash(password, SALT_ROUNDS);
+}
+
+export { hashPassword };
