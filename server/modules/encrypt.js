51f20e9ad09ace064ee7a846cd0a1aa9404db467
JeaV2 <102710@glr.nl>
2026-06-18 10:39:59 +0200

    feat(auth): implement json web token generation on signup

--- server/modules/encrypt.js
+++ server/modules/encrypt.js
@@ -1,5 +1,6 @@
 import bcrypt from 'bcrypt';
 import 'dotenv/config';
+import jwt from 'jsonwebtoken';
 
 const SALT_ROUNDS = parseInt(process.env.PSWD_SALTROUNDS);
 if (isNaN(SALT_ROUNDS)) {
@@ -14,4 +15,13 @@ async function comparePassword(plainPassword, hashedPassword) {
     return await bcrypt.compare(plainPassword, hashedPassword);
 }
 
-export { hashPassword, comparePassword };
+async function signToken(payload) {
+    const secretKey = process.env.JWT_SECRET;
+    if (!secretKey) {
+        throw new Error('JWT_SECRET environment variable is not set');
+    }
+    return jwt.sign(payload, secretKey, { expiresIn: '10d' });
+}
+
+
+export { hashPassword, comparePassword, signToken };
