3799d74a70ddb95099a96214c7ab2f5f6bd0d582
TigoV2 <102575@glr.nl>
2026-06-25 11:31:52 +0200

    Will be updating the packages later / started with middleware for questions.

--- app/src/js/changequestion.js
+++ app/src/js/changequestion.js
@@ -1,24 +1,31 @@
-const casus = {
-    vraag: "Wat is de juiste manier om een brand te blussen?",
-    tekst: "De juiste manier om een brand te blussen is met een brandblussers.",
-    video: "brand_blussen"
-}
+const fetchQuestions = async () => {
+    try {
+        const response = await fetch('./questionData/...');
+        if (!response.ok) {
+            throw new Error(`Failed to fetch projects: ${response.statusText}`);
+        } const { projects } = await response.json();
+        return projects || [];
+    } catch (error) {
+        console.error('Error fetching projects:', error);
+        return [];
+    }
+};
 
-const changeQuestion = (casus, type) => {
+const changeQuestion = (questions, type) => {
     const questionElement = document.getElementById('vraag');
 
     if (type == 'video') {
         questionElement.innerHTML = `
-        <h2>${casus.vraag}</h2>
+        <h2>${questions.vraag}</h2>
         <video controls autoplay>
-            <source src="assets/videos/${casus.video}.mp4" type="video/mp4">
+            <source src="assets/videos/${questions.video}.mp4" type="video/mp4">
             Your browser does not support the video tag.
         </video>`;
     } else {
         questionElement.innerHTML = `
-        <h2>${casus.vraag}</h2>
-        <p>${casus.tekst}</p>`;
+        <h2>${questions.vraag}</h2>
+        <p>${questions.tekst}</p>`;
     }
 }
 
-changeQuestion(casus, 'video');
+changeQuestion(questions, 'video');
