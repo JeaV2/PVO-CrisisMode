0ea0e5d1b117404621087c3a4059ee1d34332b05
JeaV2 <102710@glr.nl>
2026-06-26 15:12:58 +0200

    fix(database): correct spelling of bronze in Dutch

--- server/crisimode.sql
+++ server/crisimode.sql
@@ -12,16 +12,16 @@ CREATE TABLE IF NOT EXISTS Gebruikers (
 -- Medaille table, UUID is foreign key to Gebruikers
 CREATE TABLE IF NOT EXISTS BehaaldeMedailles (
   UUID varchar(36) PRIMARY KEY NOT NULL,
-  Casus1 varchar(6) CHECK (Casus1 IN ('goud','zilver','bronze')),
-  Casus2 varchar(6) CHECK (Casus2 IN ('goud','zilver','bronze')),
-  Casus3 varchar(6) CHECK (Casus3 IN ('goud','zilver','bronze')),
-  Casus4 varchar(6) CHECK (Casus4 IN ('goud','zilver','bronze')),
-  Casus5 varchar(6) CHECK (Casus5 IN ('goud','zilver','bronze')),
-  Casus6 varchar(6) CHECK (Casus6 IN ('goud','zilver','bronze')),
-  Casus7 varchar(6) CHECK (Casus7 IN ('goud','zilver','bronze')),
-  Casus8 varchar(6) CHECK (Casus8 IN ('goud','zilver','bronze')),
-  Casus9 varchar(6) CHECK (Casus9 IN ('goud','zilver','bronze')),
-  Casus10 varchar(6) CHECK (Casus10 IN ('goud','zilver','bronze')),
+  Casus1 varchar(6) CHECK (Casus1 IN ('goud','zilver','brons')),
+  Casus2 varchar(6) CHECK (Casus2 IN ('goud','zilver','brons')),
+  Casus3 varchar(6) CHECK (Casus3 IN ('goud','zilver','brons')),
+  Casus4 varchar(6) CHECK (Casus4 IN ('goud','zilver','brons')),
+  Casus5 varchar(6) CHECK (Casus5 IN ('goud','zilver','brons')),
+  Casus6 varchar(6) CHECK (Casus6 IN ('goud','zilver','brons')),
+  Casus7 varchar(6) CHECK (Casus7 IN ('goud','zilver','brons')),
+  Casus8 varchar(6) CHECK (Casus8 IN ('goud','zilver','brons')),
+  Casus9 varchar(6) CHECK (Casus9 IN ('goud','zilver','brons')),
+  Casus10 varchar(6) CHECK (Casus10 IN ('goud','zilver','brons')),
   FOREIGN KEY (UUID) REFERENCES Gebruikers(UUID)
 );
 
@@ -31,5 +31,5 @@ INSERT INTO Gebruikers (UUID, Voornaam, Achternaam, Username, Email, Wachtwoord,
 ('123e4567-e89b-12d3-a456-426614174001', 'Jane', 'Smith', 'janesmith', 'jane@example.com', '$2b$12$pVxHzTILo/OfGm/3tFxXqe0mA.fwUUyZNHSM6ru3.qMamJiDGoOIO', NULL); --wachtwoord is "Test%5"
 
 INSERT INTO BehaaldeMedailles (UUID, Casus1, Casus2, Casus3, Casus4, Casus5, Casus6, Casus7, Casus8, Casus9, Casus10) VALUES
-('123e4567-e89b-12d3-a456-426614174000', 'goud', 'zilver', 'bronze', 'goud', 'zilver', 'bronze', 'goud', 'zilver', 'bronze', 'goud'),
-('123e4567-e89b-12d3-a456-426614174001', 'bronze', 'bronze', 'bronze', 'bronze', 'bronze', 'bronze', 'bronze', 'bronze', 'bronze', 'bronze');
+('123e4567-e89b-12d3-a456-426614174000', 'goud', 'zilver', 'brons', 'goud', 'zilver', 'brons', 'goud', 'zilver', 'brons', 'goud'),
+('123e4567-e89b-12d3-a456-426614174001', 'brons', 'brons', 'brons', 'brons', 'brons', 'brons', 'brons', 'brons', 'brons', 'brons');
