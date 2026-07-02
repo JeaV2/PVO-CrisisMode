14d6550f870a79e6f6e1754133e8f69941ebc711
Alice <jolynejanssen@icloud.com>
2026-07-02 17:38:26 +0200

    FEAT: MAde a sign-up page

---
+++ app/src/js/signup.js
@@ -0,0 +1,50 @@
+import { Preferences } from '@capacitor/preferences';
+
+document.getElementById('signup').addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const voornaam = document.getElementById("voornaam").value
+    const achternaam = document.getElementById("achternaam").value
+    const email = document.getElementById("email").value
+    const username = document.getElementById("username").value
+    const password = document.getElementById("password").value
+    const resultDiv = document.getElementById('result');
+
+    const Data = {
+        "Voornaam": voornaam,
+        "Achternaam": achternaam,
+        "Username": username,
+        "Email": email,
+        "Wachtwoord": password
+    }
+
+    try {
+        const response = await fetch('http://127.0.0.1:5500/auth/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(Data)
+        });
+
+        const result = await response.json();
+
+        if (response.ok) {
+            const token = JSON.stringify(result.token)
+                await Preferences.set({
+                    key: 'token',
+                    value: token,
+                });
+            resultDiv.textContent = 'Succes! Gebruiker geregistreerd! U kunt nu inloggen.';
+            resultDiv.className = 'success';
+            e.target.reset();
+        } else {
+            resultDiv.textContent = 'Error: ' + JSON.stringify(result, null, 2);
+            resultDiv.className = 'error';
+        }
+    } catch (error) {
+        resultDiv.textContent = 'Fetch error: ' + error.message;
+        resultDiv.className = 'error';
+    }
+
+})
